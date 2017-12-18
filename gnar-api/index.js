9ccb3365902e77c8b9aba70ab66d02315068b7ad
domJalb <dominic@steadybudget.com>
2017-12-17 20:57:43 -0800

    link express & db

--- gnar-api/index.js
+++ gnar-api/index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 // Get our API routes
