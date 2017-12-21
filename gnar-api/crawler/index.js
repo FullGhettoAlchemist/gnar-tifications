27175b3aaffe965ff93846abe90f5d4e11b51389
domJalb <dominic@steadybudget.com>
2017-12-20 20:54:43 -0800

    raw dogs

--- gnar-api/crawler/index.js
+++ gnar-api/crawler/index.js
@@ -6,7 +6,11 @@ module.exports.crawl = function(){
     uri: "https://www.mtbachelor.com/conditions-report/",
   }, (error, response, body) => {
     let $ = cheerio.load(body);
-    let divs = $('.tab1');
+    let divs = $('.tab1').find('.list-lifts').children().eq(7).find('.statuses').eq(0).find('.status').eq(0).find('i').attr('class');
+    // let divs = $('.tab1').find('.list-lifts');
+    // for(let thing in divs){
+    //   console.log(thing);
+    // }
     console.log(divs);
   });
 }
